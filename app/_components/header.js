b2f61dff14659fa9b0c315e319ae72e4bcf77215
jaydip-changani <pjaydip956@gmail.com>
2025-09-05 13:18:10 +0530

    header added

---
+++ app/_components/header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+
+    //generate the header for my website 
+    <header className="p-4 bg-blue-400 text-white text-center">
+        <h1 className="text-3xl font-bold">Fridge Se Rasoi</h1>
+        <p className="text-lg">Cook with what you have!</p>
+        </header>
+
+);
+}
