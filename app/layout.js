0131b0aa866184f1d4f475fbb2cb7243b7f64092
jaydip-changani <pjaydip956@gmail.com>
2025-09-05 16:59:28 +0530

    SEO Friendly

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,33 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+      <title>Fridge Se Rasoi</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content="Fridge Se Rasoi - AI-Powered Recipe Generator" />
+      <meta name="keywords" content="AI, Recipes, Cooking, Ingredients, Food, Gemini API, Next.js, Jaydip, Jaydip Changani, Changani" />
+      <meta name="author" content="Jaydip Changani" />
+      <meta property="og:title" content="Fridge Se Rasoi" />
+      <meta property="og:description" content="AI-Powered Recipe Generator" />
+      <meta property="og:image" content="/og-image.png" />
+      <meta property="og:url" content="https://fridge-se-rasoi.vercel.app/" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Fridge Se Rasoi" />
+      <meta name="twitter:description" content="AI-Powered Recipe Generator" />
+      <meta name="twitter:image" content="/og-image.png" />
+      <meta name="google-site-verification" content="https://fridge-se-rasoi.vercel.app/" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Geist+Mono&family=Geist+Sans:wght@400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
+    
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
