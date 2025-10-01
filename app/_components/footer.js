61c9e5b2d03b8b824a86fd107bca92fd427ebcc9
jaydip-changani <pjaydip956@gmail.com>
2025-10-01 13:20:51 +0530

    portfolio aaded in footer

--- app/_components/footer.js
+++ app/_components/footer.js
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Footer() {
     return (
         <footer className="fixed bottom-0 left-0 w-full p-4 bg-gray-200 text-center">
-            <p className="text-sm text-gray-600">&copy; 2025 Fridge Se Rasoi. All rights reserved.</p>
+            <p className="text-sm text-gray-600">&copy; 2025 <Link href={"https://jaydip-next-js-portfolio.vercel.app/"}> Jaydip Changani.</Link> All rights reserved.</p>
         </footer>
     );
 }
